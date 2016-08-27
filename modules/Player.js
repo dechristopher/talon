15e5fb93908ce22d33d6d5e16acf046b01143394
Andrew DeChristopher <dechristophera@wit.edu>
2016-08-27 12:03:40 -0400

    Removed unused functionality in modules/Player.js

--- modules/Player.js
+++ modules/Player.js
@@ -1,22 +1,12 @@
 /**
  * Created by Drew on 4/22/2016.
  */
-// Private
-var playerCount = 0;
-
-function depositMinusFee(num) {
-    return num - 0.1;
-}
-
-// Public
 module.exports = Player;
 
 function Player(n, s, c) {
-    //this.id = playerCount;
     this.nm = n;
     this.sid = s;
     this.channel = c;
-    playerCount++;
 }
 
 Player.prototype.getName = function() {
