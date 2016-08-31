80728198e611f864b1bcf7f3a8f30f4459fe6bce
Andrew DeChristopher <dechristophera@wit.edu>
2016-08-31 01:08:44 -0400

    Beautified demo/demo-client.js

--- demo/demo-client.js
+++ demo/demo-client.js
@@ -2,16 +2,20 @@
 Created by Andrew DeChristopher <drew@kiir.us> on 8/30/2016.
  */
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:8080', {reconnect: true});
+const socket = io.connect('http://localhost:8080', {
+    reconnect: true
+});
 const fs = require('fs');
 const ss = require('socket.io-stream');
 
-socket.on( 'connect', function() {
-  console.log( "Sockets connected" );
+socket.on('connect', function() {
+    console.log("Sockets connected");
 
-  var stream = ss.createStream();
-  var filename = 'kiwi-25.dem';
+    var stream = ss.createStream();
+    var filename = 'kiwi-25.dem';
 
-  ss(socket).emit('foo', stream, {name: filename});
-  fs.createReadStream(filename).pipe(stream);
+    ss(socket).emit('foo', stream, {
+        name: filename
+    });
+    fs.createReadStream(filename).pipe(stream);
 });
