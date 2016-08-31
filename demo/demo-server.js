e7fb8281df922ac1a4f8124341390dafc9d220b5
Andrew DeChristopher <dechristophera@wit.edu>
2016-08-31 16:01:29 -0400

    Feeble attempt at redacting windows directory structure down to filename

--- demo/demo-server.js
+++ demo/demo-server.js
@@ -10,7 +10,8 @@ io.sockets.on('connection', function(socket) {
     console.log('client connected');
 
     ss(socket).on('foo', function(stream, data) {
-        var filename = path.basename(data.name);
+        var filename = path.basename(data.name).split('\\');
+        var filename = filename[filename.length - 1];
         stream.pipe(fs.createWriteStream('www/demo/' + filename));
     });
 });
