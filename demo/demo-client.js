e4372707370ca2611b09f2260ee5412789a364d6
Andrew DeChristopher <dechristophera@wit.edu>
2016-08-31 15:42:42 -0400

    Implemented demo folder population and basic testing to demo-client

--- demo/demo-client.js
+++ demo/demo-client.js
@@ -2,20 +2,47 @@
 Created by Andrew DeChristopher <drew@kiir.us> on 8/30/2016.
  */
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:8080', {
+const socket = io.connect('http://localhost:27000', {
     reconnect: true
 });
-const fs = require('fs');
 const ss = require('socket.io-stream');
+const cron = require('cron');
+const fs = require('fs');
+const ArrayList = require("arraylist");
+const lupus = require('lupus');
+const HashMap = require("hashmap");
+const requestify = require('requestify');
+
+var demoFolders = new ArrayList();
+
+populateServers("conf/demo.txt", demoFolders);
 
 socket.on('connect', function() {
     console.log("Sockets connected");
 
     var stream = ss.createStream();
-    var filename = 'kiwi-25.dem';
+    var filename = 'C:\KIWI\kp1\demos\kiwi-3.dem';
 
     ss(socket).emit('foo', stream, {
         name: filename
     });
     fs.createReadStream(filename).pipe(stream);
 });
+
+//Fill the servers[] array with lines from
+//a given text file
+function populateDemoFolders(file, list) {
+    fs.exists(file, function(exists) {
+        if (exists) {
+            fs.readFileSync(file).toString().split('\n').forEach(function(line) {
+                if (line !== '') {
+                    list.add(line);
+                }
+            });
+            console.log('FILLED:', list);
+        } else {
+            log('DEMO LOCATION FILE DOES NOT EXIST! HALTING');
+            process.exit();
+        }
+    });
+}
