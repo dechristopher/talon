d81496440100e65e40613f976399acfe8464b4e4
Andrew DeChristopher <dechristophera@wit.edu>
2016-09-01 02:27:22 -0400

    Added logging constants, implemented ext logging, made on DC event

--- demo/demo-client.js
+++ demo/demo-client.js
@@ -20,44 +20,55 @@ const requestify = require('requestify');
 const gutil = require('gulp-util');
 const os = require('os');
 
+//talon libraries
+const log = require('../modules/log');
+
 //Define ERORS and other constants
 const ERROR_NO_FOLDER = '[' + gutil.colors.red('ERROR') + '] Given demo folder does not exist: ';
 const ERROR_NO_REGION = '[' + gutil.colors.red('ERROR') + '] Please use a package.json script to define region.';
-const DEMO = '[' + gutil.colors.red('DEMO') + '] ';
+const ERROR_BAD_REGION = '[' + gutil.colors.red('ERROR') + '] Invalid region specified.';
+const DEMO = '[' + gutil.colors.green('DEMO') + '] ';
+const DEMO_RED = '[' + gutil.colors.red('DEMO') + '] ';
 
 var demoFolders = new ArrayList();
 
 const usnj1sock = 'us-nj1';
 const usnj2sock = 'us-nj2';
 var sock = '';
+var go = false;
+
+log(DEMO + 'KIWI Demo Uploader - initializing..', '-demo-client');
 
 populateDemoFolders("conf/demo.txt", demoFolders);
 
 //Set correct socket.io channel
 if (process.argv.length > 2) {
-    if (process.argv[2] == "dev") {
-        log(DEMO + 'Starting US-NJ1 Uploader');
+    if (process.argv[2] == "us-nj1") {
+        log(DEMO + 'Starting US-NJ1 Uploader', '-demo-client');
         sock = usnj1sock;
-    } else {
-        log(DEMO + 'Starting US-NJ2 Uploader');
+    } else if (process.argv[2] == "us-nj2") {
+        log(DEMO + 'Starting US-NJ2 Uploader', '-demo-client');
         sock = usnj2sock;
+    } else {
+        throw new Error(ERROR_BAD_REGION);
     }
 } else {
     throw new Error(ERROR_NO_REGION);
 }
 
 socket.on('connect', function() {
-    console.log("Sockets connected");
-
-    var stream = ss.createStream();
-    var filename = 'C:\\KIWI\\kp1\\csgo\\demos\\kiwi-43.dem';
-
-    ss(socket).emit(sock, stream, {
-        name: filename
-    });
-
-    fs.createReadStream(filename).pipe(stream);
+    console.log(DEMO + "Connected to demo CDN.");
+    go = true;
 });
+
+socket.on('disconnect', function() {
+    console.log(DEMO_RED + "Disconnected from demo CDN.");
+    go = false;
+});
+
+function finished(filename){
+    log(DEMO + ' DONE -> ' + filename, '-demo-client');
+}
 
 //Fill the servers[] array with lines from
 //a given text file
