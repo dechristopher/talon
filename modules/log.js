135f5c5b12844c89f68486bccea1a634865150c0
Andrew DeChristopher <dechristophera@wit.edu>
2016-09-01 02:25:07 -0400

    Ported logging to external module (modules/log.js)

---
+++ modules/log.js
@@ -0,0 +1,35 @@
+const datetime = require('node-datetime');
+const fs = require('fs');
+const os = require('os');
+
+//Wraps console.log for printing date in front
+function log(message, logname) {
+    var time = datetime.create().format('m-d-y H:M:S');
+    var today = datetime.create().format('m-d-y');
+    var file = "";
+    if(logname === null){
+        file = "logs/" + today + ".log";
+    }else{
+        file = "logs/" + today + logname + ".log";
+    }
+    console.log('[' + time + '] ' + message);
+
+    fs.exists(file, function(exists) {
+        if (exists) {
+            fs.appendFile(file, '[' + time + '] ' + message + os.EOL, function(err) {
+                if (err) {
+                    return console.log("FILE LOGGING FAILED AT " + time + "for MSG: " + message);
+                }
+            });
+        } else {
+            fs.writeFile(file, 'BEGIN TALON LOG FOR ' + today + os.EOL, function(err) {
+                if (err) {
+                    return console.log("FILE CREATION FAILED AT " + time + "for FILE: " + file);
+                }
+                console.log('Created new log >> ' + file);
+            });
+        }
+    });
+}
+
+module.exports = log;
