2f0597edb933f92e960132938195383facfefe80
Andrew DeChristopher <dechristophera@wit.edu>
2016-09-06 07:56:56 -0400

    Set up module logging in demo/demo-server.js

--- demo/demo-server.js
+++ demo/demo-server.js
@@ -4,10 +4,14 @@ Created by Andrew DeChristopher <drew@kiir.us> on 8/30/2016.
 const io = require('socket.io').listen(27000);
 const ss = require('socket.io-stream');
 const fs = require('fs');
-var path = require('path');
+const path = require('path');
+
+const log = require('../modules/log');
+
+log('[KIWI] Demo CDN Server', '-demo-server');
 
 io.sockets.on('connection', function(socket) {
-    console.log('client connected');
+    log('Client connected', '-demo-server');
     //For uploads from US-NJ1
     ss(socket).on('us-nj1', function(stream, data) {
         var filename = path.basename(data.name).split('\\');
