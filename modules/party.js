db8427ca85b79b5cd41d95ccaea7e451900480e0
Andrew DeChristopher <dechristophera@wit.edu>
2016-09-11 11:32:54 -0400

    Rewrote and commented out in-module testing

--- modules/party.js
+++ modules/party.js
@@ -216,11 +216,26 @@ function deleteParty(party, rcon, callback) {
 
 }
 
-/*var testuser = user('testuser', 'STEAM_0:0:TESTING', 'TESTCHANNEL', 'OFI)*#BO*QBO@CLIb');
-
+/*
+var testuser = user('testuser', 'STEAM_0:0:TESTING', 'TESTCHANNEL', 'OFI)*#BO*QBO@CLIb');
 console.log(testuser.getUsername() + ' - ' + testuser.getSteamID() + ' - ' + testuser.getChannel() + ' - ' + testuser.getHWID());
+*/
 
-createParty(testuser.getUsername(), rcon, function(id) {
-    console.log(SUCCESS + 'Created party: ' + id);
-    process.exit();
+/*rcon = redis.createClient(6379, 'kiir.us');
+const auth = "KIWICLIENTREDISPASSWORDTHATISWAYTOOLONGTOGUESSBUTSTILLFEASIBLETOGETBYDECRYPTINGOURCLIENTSOKUDOSTOYOUIFYOUDIDLOLJKPLEASETELLUSTHISISSCARY";
+rcon.auth(auth);
+
+rcon.select(2, function(err, res) {
+    if (err == undefined) {
+        console.log('Selected DB2');
+        getPlayersInParty('party:KTSotnN', rcon, function(reply){
+            console.log(reply);
+            reply.forEach(function(value) {
+              console.log(' - ' + value);
+            });
+            rcon.quit();
+        });
+    } else {
+        throw new Error(err);
+    }
 });*/
