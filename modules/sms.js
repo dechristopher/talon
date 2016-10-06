d2fae565bcb02e10170a5622c81136eedf279627
Andrew DeChristopher <dechristophera@wit.edu>
2016-10-06 07:52:33 -0400

    Beautified modules/sms.js

--- modules/sms.js
+++ modules/sms.js
@@ -7,7 +7,7 @@ module.exports.sendSms = function(to, message) {
         body: message,
         to: to,
         from: cfg.sendingNumber
-        //mediaUrl: 'http://www.yourserver.com/someimage.png'
+            //mediaUrl: 'http://www.yourserver.com/someimage.png'
     }, function(err, data) {
         if (err) {
             console.error('Could not send SMS.');
