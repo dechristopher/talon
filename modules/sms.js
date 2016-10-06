17f7345bc7dbbccf559d184cc252fa4393b063d6
Andrew DeChristopher <dechristophera@wit.edu>
2016-10-06 07:52:18 -0400

    Implemented sendAdminSms to send SMS to adminNumbers in global config

--- modules/sms.js
+++ modules/sms.js
@@ -17,3 +17,21 @@ module.exports.sendSms = function(to, message) {
         }
     });
 };
+
+module.exports.sendAdminSms = function(message) {
+    cfg.adminNumbers.forEach(function(entry) {
+        client.messages.create({
+            body: message,
+            to: entry,
+            from: cfg.sendingNumber
+                //mediaUrl: 'http://www.yourserver.com/someimage.png'
+        }, function(err, data) {
+            if (err) {
+                console.error('Could not send SMS.');
+                console.error(err);
+            } else {
+                //log('[SMS] Talon Started!');
+            }
+        });
+    }));
+};
