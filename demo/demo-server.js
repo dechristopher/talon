c29e0160596e0d7bd40322a3b78f7382174a1abe
Andrew DeChristopher <dechristophera@wit.edu>
2016-10-07 17:40:49 -0400

    Print filesize of demos using size parameter sent from demo-clients

--- demo/demo-server.js
+++ demo/demo-server.js
@@ -32,14 +32,15 @@ io.sockets.on('connection', function(socket) {
     ss(socket).on('us-nj1', function(stream, data) {
         var filename = path.basename(data.name).split('\\');
         var filename = filename[filename.length - 1];
-        log(DEMO + 'Uploading: ' + filename);
+        var filesize = data.size;
+        log(DEMO + 'Uploading: ' + filename + '~' + filesize + 'B', '-demo-server');
         stream.pipe(fs.createWriteStream(cfg.demoDir + filename));
     });
     //For uploads from US-NJ2
     ss(socket).on('us-nj2', function(stream, data) {
         var filename = path.basename(data.name).split('\\');
         var filename = filename[filename.length - 1];
-        log(DEMO + 'Uploading: ' + filename);
+        log(DEMO + 'Uploading: ' + filename + '~' + filesize + 'B', '-demo-server');
         stream.pipe(fs.createWriteStream(cfg.demoDir + filename));
     });
 });
