a21e4b107b0fc3a6612e1f4d5c9bca0a7efa06d0
Andrew DeChristopher <dechristophera@wit.edu>
2016-10-07 18:05:25 -0400

    Fixed logging format for demo uploading

--- demo/demo-server.js
+++ demo/demo-server.js
@@ -33,14 +33,14 @@ io.sockets.on('connection', function(socket) {
         var filename = path.basename(data.name).split('\\');
         var filename = filename[filename.length - 1];
         var filesize = data.size;
-        log(DEMO + 'Uploading: ' + filename + '~' + filesize + 'B', '-demo-server');
+        log(DEMO + 'Uploading: ' + filename + ' ~' + filesize + ' B', '-demo-server');
         stream.pipe(fs.createWriteStream(cfg.demoDir + filename));
     });
     //For uploads from US-NJ2
     ss(socket).on('us-nj2', function(stream, data) {
         var filename = path.basename(data.name).split('\\');
         var filename = filename[filename.length - 1];
-        log(DEMO + 'Uploading: ' + filename + '~' + filesize + 'B', '-demo-server');
+        log(DEMO + 'Uploading: ' + filename + ' ~' + filesize + ' B', '-demo-server');
         stream.pipe(fs.createWriteStream(cfg.demoDir + filename));
     });
 });
