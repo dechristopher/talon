0765ade4ec893ac4ed6c37e2f25535903f5ce9c0
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-09 00:07:41 -0500

    Added consoleOut argument to log.js to only print to file

--- modules/log.js
+++ modules/log.js
@@ -10,11 +10,16 @@ const os = require('os');
 const LOG = '[' + gutil.colors.blue('LOG') + '] ';
 
 //Wraps console.log for printing date in front
-function log(message, logname) {
+//message: (string) message to log
+//logname: (string) name of sub-log file (defaults to global log file)
+//consoleOut: (bool) whether or not to print log to console (defaults to true)
+function log(message, logname, consoleOut) {
     var time = datetime.create().format('m-d-y H:M:S');
     var today = datetime.create().format('m-d-y');
     var file = '';
     var line = '';
+
+    //Handle logname argument
     if(logname === undefined){
         file = 'logs/' + today + '.log';
 		line = '[' + time + ']' + message;
@@ -23,7 +28,14 @@ function log(message, logname) {
 		line = '[' + time + ']' /*+ [' + logname + ']*/ + message;
     }
 
-    console.log(line);
+	//Handle consoleOut argument
+	if(consoleOut === undefined) {
+		console.log(line);
+	} else if(consoleOut === true) {
+		console.log(line);
+	} else if(consoleOut === false) {
+		//Do nothing
+	}
 
     fs.exists(file, function(exists) {
         if (exists) {
