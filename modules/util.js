562199ad7c40abeb6d4dec160a48806d871db29d
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-09 00:04:15 -0500

    Added missing semicolons throughout util.js module functions

--- modules/util.js
+++ modules/util.js
@@ -24,29 +24,29 @@ util.ascii = function() {
 	log(TALN + '   ░        ░   ▒     ░ ░   ░ ░ ░ ▒     ░   ░ ░ ');
 	log(TALN + '                ░  ░    ░  ░    ░ ░           ░ ');
 	log(TALN);
-}
+};
 
 //Generate a random integer within
 //an interval inclusively
 util.random = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 //Wrapper for indexOf
 //Checks if a string A contains an
 //instance of string B
 util.contains = function(a, b) {
     return str(a).contains(b);
-}
+};
 
 //return steamid64 representation of normal steamid
 util.sidTo64 = function(steamid) {
     return sidconvert.convertTo64(steamid);
-}
+};
 
 //Returns an asterisk if true
 util.boolStar = function(bool) {
     return (bool ? "*" : "");
-}
+};
 
 module.exports = util;
