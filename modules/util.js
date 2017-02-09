a8e7b11080a933ee22e34f5c2499949928d20b4c
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-09 00:30:37 -0500

    We don't need sanity checks

--- modules/util.js
+++ modules/util.js
@@ -40,7 +40,7 @@ util.contains = function(a, b) {
 };
 
 util.stringToBool = function(string){
-    switch(string.toLowerCase().trim()){
+    switch(string){
         case "true": case "yes": case "1": return true;
         case "false": case "no": case "0": case null: return false;
         default: return Boolean(string);
