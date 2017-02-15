6ee2bd6610e67283cdb59da5b8fae9a1e33e214e
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-15 00:15:28 -0500

    Print newline after message initialization in msg.js

--- modules/msg.js
+++ modules/msg.js
@@ -7,6 +7,7 @@ module.exports = function(dev, backend, auth) {
 	//core libraries
 	const redis = require('redis');
 	const gutil = require('gulp-util');
+	const os = require('os');
 
 	//import configuration
 	const log = require('./log');
@@ -49,6 +50,8 @@ module.exports = function(dev, backend, auth) {
 	    }
 	    cli.quit();
 	};
+
+	log('[' + gutil.colors.yellow('MESG') + '] Messaging initialized' + os.EOL);
 
 	return m;
 };
