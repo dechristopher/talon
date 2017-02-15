bc1a7d053f86b0dc3219f046b7dc7553b9914f12
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-15 02:12:08 -0500

    XO --fix on player.js

--- modules/player.js
+++ modules/player.js
@@ -5,19 +5,19 @@ Created by Andrew DeChristopher <drew@kiir.us> on 4/22/2016.
 module.exports = Player;
 
 function Player(n, s, c) {
-    this.nm = n;
-    this.sid = s;
-    this.channel = c;
+	this.nm = n;
+	this.sid = s;
+	this.channel = c;
 }
 
-Player.prototype.getName = function() {
-    return this.nm;
+Player.prototype.getName = function () {
+	return this.nm;
 };
 
-Player.prototype.getSid = function() {
-    return this.sid;
+Player.prototype.getSid = function () {
+	return this.sid;
 };
 
-Player.prototype.getChannel = function() {
-    return this.channel;
+Player.prototype.getChannel = function () {
+	return this.channel;
 };
