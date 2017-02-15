060c18b7b233d8afb03c9985da1f4d88baf1e15d
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-15 02:38:33 -0500

    XO --fix util.js

--- modules/util.js
+++ modules/util.js
@@ -2,19 +2,19 @@
 Created by Andrew DeChristopher <drew@kiir.us> on 1/7/2017.
  */
 
-//core libraries
-const log = require('./log');
-const gutil = require('gulp-util');
+// core libraries
 const str = require('string');
+const gutil = require('gulp-util');
 const sidconvert = require('steamidconvert')();
+const log = require('./log');
 
-//logging constants
+// logging constants
 const TALN = '[' + gutil.colors.magenta('TALN') + '] ';
 
-//Export object
+// Export object
 var util = {};
 
-util.ascii = function() {
+util.ascii = function () {
 	log(TALN);
 	log(TALN + ' ▄▄▄█████▓    ▄▄▄          ██▓        ▒█████      ███▄    █             ▄▄▄██▀▀▀     ██████ ');
 	log(TALN + ' ▓  ██▒ ▓▒   ▒████▄       ▓██▒       ▒██▒  ██▒    ██ ▀█   █               ▒██      ▒██    ▒ ');
@@ -29,88 +29,93 @@ util.ascii = function() {
 	log(TALN);
 };
 
-//Generate a random integer within
-//an interval inclusively
-util.random = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+// Generate a random integer within
+// an interval inclusively
+util.random = function (min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-//Wrapper for indexOf
-//Checks if a string A contains an
-//instance of string B
-util.contains = function(a, b) {
-    return str(a).contains(b);
+// Does absolutely nothing
+util.devnull = function (data) {
+	return data;
 };
 
-util.stringToBool = function(string){
-    switch(string){
-        case "true": case "yes": case "1": return true;
-        case "false": case "no": case "0": case null: return false;
-        default: return Boolean(string);
-    }
+// Wrapper for indexOf
+// Checks if a string A contains an
+// instance of string B
+util.contains = function (a, b) {
+	return str(a).contains(b);
 };
 
-//return steamid64 representation of normal steamid
-util.sidTo64 = function(steamid) {
-    return sidconvert.convertTo64(steamid);
-};
-
-//Returns an asterisk if true
-util.boolStar = function(bool) {
-    return (bool ? "*" : "");
-};
-
-util.rank = function(xp) {
-	if (xp < 27) {
-		return "♟ Pawn";
-	} else if (xp >= 27 && xp < 45) {
-		return "♙ Pawn+";
-	} else if (xp >= 45 && xp < 70) {
-		return "♞ Knight";
-	} else if (xp >= 70 && xp < 105) {
-		return "♘ Knight+";
-	} else if (xp >= 105 && xp < 136) {
-		return "♝ Bishop";
-	} else if (xp >= 136 && xp < 180) {
-		return "♗ Bishop+";
-	} else if (xp >= 180 && xp < 221) {
-		return "♜ Rook";
-	} else if (xp >= 221 && xp < 270) {
-		return "♖ Rook+";
-	} else if (xp >= 270 && xp < 330) {
-		return "♛ Queen";
-	} else if (xp >= 330 && xp < 420) {
-		return "♕ Queen+";
-	} else if (xp >= 420 && xp < 461) {
-		return "♔ King";
-	} else if (xp >= 461) {
-		return "Ⓛ Legend";
+util.stringToBool = function (string) {
+	switch (string) {
+		case 'true': case 'yes': case '1': return true;
+		case 'false': case 'no': case '0': case null: return false;
+		default: return Boolean(string);
 	}
 };
 
-util.xptot = function(xp) {
+// return steamid64 representation of normal steamid
+util.sidTo64 = function (steamid) {
+	return sidconvert.convertTo64(steamid);
+};
+
+// Returns an asterisk if true
+util.boolStar = function (bool) {
+	return (bool ? '*' : '');
+};
+
+util.rank = function (xp) {
 	if (xp < 27) {
-		return xp + " / 27";
+		return '♟ Pawn';
 	} else if (xp >= 27 && xp < 45) {
-		return xp + " / 45";
+		return '♙ Pawn+';
 	} else if (xp >= 45 && xp < 70) {
-		return xp + " / 70";
+		return '♞ Knight';
 	} else if (xp >= 70 && xp < 105) {
-		return xp + " / 205";
+		return '♘ Knight+';
 	} else if (xp >= 105 && xp < 136) {
-		return xp + " / 136";
+		return '♝ Bishop';
 	} else if (xp >= 136 && xp < 180) {
-		return xp + " / 180";
+		return '♗ Bishop+';
 	} else if (xp >= 180 && xp < 221) {
-		return xp + " / 221";
+		return '♜ Rook';
 	} else if (xp >= 221 && xp < 270) {
-		return xp + " / 270";
+		return '♖ Rook+';
 	} else if (xp >= 270 && xp < 330) {
-		return xp + " / 330";
+		return '♛ Queen';
 	} else if (xp >= 330 && xp < 420) {
-		return xp + " / 420";
+		return '♕ Queen+';
 	} else if (xp >= 420 && xp < 461) {
-		return xp + " / 460";
+		return '♔ King';
+	} else if (xp >= 461) {
+		return 'Ⓛ Legend';
+	}
+};
+
+util.xptot = function (xp) {
+	if (xp < 27) {
+		return xp + ' / 27';
+	} else if (xp >= 27 && xp < 45) {
+		return xp + ' / 45';
+	} else if (xp >= 45 && xp < 70) {
+		return xp + ' / 70';
+	} else if (xp >= 70 && xp < 105) {
+		return xp + ' / 205';
+	} else if (xp >= 105 && xp < 136) {
+		return xp + ' / 136';
+	} else if (xp >= 136 && xp < 180) {
+		return xp + ' / 180';
+	} else if (xp >= 180 && xp < 221) {
+		return xp + ' / 221';
+	} else if (xp >= 221 && xp < 270) {
+		return xp + ' / 270';
+	} else if (xp >= 270 && xp < 330) {
+		return xp + ' / 330';
+	} else if (xp >= 330 && xp < 420) {
+		return xp + ' / 420';
+	} else if (xp >= 420 && xp < 461) {
+		return xp + ' / 460';
 	} else if (xp >= 461) {
 		return xp;
 	}
