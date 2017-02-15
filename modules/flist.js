8e4f4d7f15bc0fd0d334c5d1c75aca10cd1307b8
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-15 02:39:13 -0500

    XO --fix flist.js

--- modules/flist.js
+++ modules/flist.js
@@ -3,28 +3,28 @@ Created by Andrew DeChristopher <drew@kiir.us> on 1/5/2017.
  */
 
 const fs = require('fs');
-const log = require('./log');
 const gutil = require('gulp-util');
+const log = require('./log');
 
 const TALN = '[' + gutil.colors.magenta('TALN') + '] ';
 
 var flist = {};
 
-//Fill an arraylist list with lines from a file
-flist.fill = function(file, list, numSrv, displayname, err) {
-    fs.exists(file, function(exists) {
-        if (exists) {
-            fs.readFileSync(file).toString().split('\n').forEach(function(line) {
-                if (line !== '') {
-                    list.add(line);
-                }
-            });
-            log(TALN + 'FILLED ' + displayname + ': [ ' + list + ' ]');
+// Fill an arraylist list with lines from a file
+flist.fill = function (file, list, numSrv, displayname, err) {
+	fs.exists(file, function (exists) {
+		if (exists) {
+			fs.readFileSync(file).toString().split('\n').forEach(function (line) {
+				if (line !== '') {
+					list.add(line);
+				}
+			});
+			log(TALN + 'FILLED ' + displayname + ': [ ' + list + ' ]');
 			numSrv = list.size();
-        } else {
-            throw new Error(err + file);
-        }
-    });
+		} else {
+			throw new Error(err + file);
+		}
+	});
 };
 
 module.exports = flist;
