f55c59a8b134e4652884829e7837280397a9409c
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-15 02:40:10 -0500

    XO --fix sms.js

--- modules/sms.js
+++ modules/sms.js
@@ -2,40 +2,47 @@
 Created by Andrew DeChristopher <drew@kiir.us> on 10/5/2016.
  */
 
+let client = require('twilio');
+const gutil = require('gulp-util');
 const cfg = require('./cfg');
 const log = require('./log');
-const client = require('twilio')(cfg.accountSid, cfg.authToken);
 
-module.exports.sendSms = function(to, message) {
-    client.messages.create({
-        body: message,
-        to: to,
-        from: cfg.sendingNumber
-            //mediaUrl: 'http://www.yourserver.com/someimage.png'
-    }, function(err, data) {
-        if (err) {
-            console.error('Could not send SMS.');
-            console.error(err);
-        } else {
-            //log('[SMS] Talon Started!');
-        }
-    });
+const SMS = '[' + gutil.colors.green('SMS') + '] ';
+
+client = client(cfg.accountSid, cfg.authToken);
+
+module.exports.sendSms = function (to, message) {
+	client.messages.create({
+		body: message,
+		to: to,
+		from: cfg.sendingNumber
+            // mediaUrl: 'http://www.yourserver.com/someimage.png'
+	}, function (err, data) {
+		if (err) {
+			console.error('Could not send SMS.');
+			console.error(err, data);
+		} else {
+			log(SMS + to + ' : ' + message);
+            // log('[SMS] Talon Started!');
+		}
+	});
 };
 
-module.exports.sendAdminSms = function(message) {
-    cfg.adminNumbers.forEach(function(entry) {
-        client.messages.create({
-            body: message,
-            to: entry,
-            from: cfg.sendingNumber
-                //mediaUrl: 'http://www.yourserver.com/someimage.png'
-        }, function(err, data) {
-            if (err) {
-                console.error('Could not send SMS.');
-                console.error(err);
-            } else {
-                //log('[SMS] Talon Started!');
-            }
-        });
-    });
+module.exports.sendAdminSms = function (message) {
+	cfg.adminNumbers.forEach(function (entry) {
+		client.messages.create({
+			body: message,
+			to: entry,
+			from: cfg.sendingNumber
+                // mediaUrl: 'http://www.yourserver.com/someimage.png'
+		}, function (err, data) {
+			if (err) {
+				console.error('Could not send SMS.');
+				console.error(err, data);
+			} else {
+				log(SMS + 'ADMIN : ' + message);
+                // log('[SMS] Talon Started!');
+			}
+		});
+	});
 };
