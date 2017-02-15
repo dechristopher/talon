b6de3cae8a9b6831c869b95a0059e3032db8e2fa
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-15 02:39:34 -0500

    XO --fix match.js

--- modules/match.js
+++ modules/match.js
@@ -3,66 +3,66 @@ Created by Andrew DeChristopher <drew@kiir.us> on 1/25/2017.
  */
 
 const match = (mid, mip, pw, t1n, t2n, pl) => {
-    //ID of the match
-    var matchid = Object.undefined;
-    if (mid !== "") {
-        matchid = mid;
-    }
+    // ID of the match
+	var matchid = Object.undefined;
+	if (mid !== '') {
+		matchid = mid;
+	}
 
-	//IP of match server
+	// IP of match server
 	var matchip = Object.undefined;
-    if (mip !== "") {
-        matchip = mip;
-    }
+	if (mip !== '') {
+		matchip = mip;
+	}
 
-	//Password of match server
+	// Password of match server
 	var matchpw = Object.undefined;
-    if (pw !== "") {
-        matchpw = pw;
-    }
+	if (pw !== '') {
+		matchpw = pw;
+	}
 
-    //Name of team one
-    var teamOneName = Object.undefined;
-    if (t1n !== "") {
-        teamOneName = t1n;
-    }
+    // Name of team one
+	var teamOneName = Object.undefined;
+	if (t1n !== '') {
+		teamOneName = t1n;
+	}
 
-    //Name of team two
-    var teamTwoName = Object.undefined;
-    if (t2n !== "") {
-        teamTwoName = t2n;
-    }
+    // Name of team two
+	var teamTwoName = Object.undefined;
+	if (t2n !== '') {
+		teamTwoName = t2n;
+	}
 
-    //Array of player objects
-    var players = Object.undefined;
-    if (pl !== "" && pl !== []) {
-        players = pl;
-    }
+    // Array of player objects
+	var players = Object.undefined;
+	if (pl !== '' && pl !== []) {
+		players = pl;
+	}
 
-    //Return anonymous functions as "internal methods"
-    return {
-        getMatchID: () => {
-            return matchid;
-        },
+    // Return anonymous functions as "internal methods"
+	return {
+		getMatchID: () => {
+			return matchid;
+		},
 		getMatchIP: () => {
-            return matchip;
-        },
+			return matchip;
+		},
 		getMatchPassword: () => {
-            return matchpw;
-        },
-        getTeamOneName: () => {
-            return teamOneName;
-        },
-        getTeamTwoName: () => {
-            return teamTwoName;
-        },
-        getPlayers: () => {
-            return players;
-        },
+			return matchpw;
+		},
+		getTeamOneName: () => {
+			return teamOneName;
+		},
+		getTeamTwoName: () => {
+			return teamTwoName;
+		},
+		getPlayers: () => {
+			return players;
+		},
 		getNumPlayers: () => {
 			return players.length;
 		}
-    };
+	};
 };
 
 module.exports = match;
