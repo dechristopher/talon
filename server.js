c69abf5070c5b766b650c764751fe4e2f73fe8d4
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-15 00:17:02 -0500

    Removed newline from listener success in server.js

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ inm.on('connect', function() {
 //Subscribe local talon redis client to global message queue
 inm.on('subscribe', function(channel, count) {
     pList.set("talon", new player("talon", "STEAM_0:1:39990", "ThIsIsAcHaNnElId"));
-    log(TALN + "Connected! Listening: " + channel + os.EOL);
+    log(TALN + "Connected! Listening: " + channel);
 
 	msg = msg(cfg.dev, cfg.backend, cfg.auth);
     //msg.reply('talon', 'testing');
