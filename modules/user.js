64ac5302841c9f50d3ceb4aa6e5d4e49e3247303
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-15 02:40:21 -0500

    XO --fix user.js

--- modules/user.js
+++ modules/user.js
@@ -3,36 +3,36 @@ Created by Andrew DeChristopher <drew@kiir.us> on 6/1/2016.
  */
 
 const user = (u, s, c, h) => {
-    var username = Object.undefined;
-    if (u !== "") {
-        username = u;
-    }
-    var steamid = Object.undefined;
-    if (s !== "") {
-        steamid = s;
-    }
-    var channel = Object.undefined;
-    if (c !== "") {
-        channel = c;
-    }
-    var hwid = Object.undefined;
-    if (c !== "") {
-        hwid = h;
-    }
-    return {
-        getUsername: () => {
-            return username;
-        },
-        getSteamID: () => {
-            return steamid;
-        },
-        getChannel: () => {
-            return channel;
-        },
-        getHWID: () => {
-            return hwid;
-        }
-    };
+	var username = Object.undefined;
+	if (u !== '') {
+		username = u;
+	}
+	var steamid = Object.undefined;
+	if (s !== '') {
+		steamid = s;
+	}
+	var channel = Object.undefined;
+	if (c !== '') {
+		channel = c;
+	}
+	var hwid = Object.undefined;
+	if (c !== '') {
+		hwid = h;
+	}
+	return {
+		getUsername: () => {
+			return username;
+		},
+		getSteamID: () => {
+			return steamid;
+		},
+		getChannel: () => {
+			return channel;
+		},
+		getHWID: () => {
+			return hwid;
+		}
+	};
 };
 
 module.exports = user;
