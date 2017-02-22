28f11aed349f3f32bcc647edea290fe64ca30afc
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-22 18:14:31 -0500

    OVERHAUL match tracking system, fixed errors and completed functionality

--- modules/matches.js
+++ modules/matches.js
@@ -7,15 +7,15 @@ const cron = require('cron');
 const c = require('chalk');
 const HashMap = require('hashmap');
 const r = require('requestify');
+const util = require('util');
 
 // import configuration
 const cfg = require('./cfg');
 
 // custom libraries
 const log = require('./log');
-const util = require('./util');
-// const match = require('./match');
-const msg = require('./msg');
+const tutil = require('./util');
+const msg = require('./msg')(cfg.dev, cfg.backend, cfg.auth);
 
 // logging constants
 const M = '[' + c.blue('MTCH') + '] ';
@@ -37,38 +37,51 @@ m.jobCheckActive = cron.job('*/5 * * * * *', function () {
 	}
 	m.active.forEach(function (value, key) {
 		let endpoint = util.format(cfg.endpoints.serverQuery, cfg.api, key);
+		// log(M + 'checking: ' + key + ' - ' + endpoint);
 		r.get(endpoint).then(response => parseStatus(response));
 	});
 });
 
+// Checks active match servers for match end
+m.jobListActive = cron.job('*/45 * * * * *', function () {
+	log(M + m.active.count() + ' Running Matches');
+});
+
+
 // Parse API response and verify match end
 function parseStatus(response) {
-	let r;
+	let si;
 	let hostname;
 	let ipp;
 	let players;
-	if (util.contains(response.getBody(), '~')) {
-		r = response.getBody().split('~');
-		ipp = r[0];
-		hostname = r[1];
-		players = r[2];
+	if (tutil.contains(response.getBody(), '~')) {
+		si = response.getBody().split('~');
+		ipp = si[0];
+		hostname = si[1];
+		players = si[2];
 
 		if (cfg.debug) {
 			log('matches.js -> m.parseStatus(' + ipp + ', ' + hostname + ', ' + players + ')', 'debug');
 		}
 
+		// log('matches.js -> m.parseStatus(' + ipp + ', ' + hostname + ', ' + players + ')', 'debug');
+
         // Server has restarted, thusly match has ended
 		// 108.61.129.168:27015~KIWI::OFF~0
-		if (hostname === 'KIWI::OFF' && players === '1' /* && !onlServers.contains(ip) */ && hostname !== 'KIWI::LIVE') {
+		if (hostname === 'KIWI::OFF' && players === '1' && hostname !== 'KIWI::LIVE') {
+			// log(M + 'met criteria');
 			// broadcast match over to all players in match
-			let match = m.active.get(ipp);
-			let matchid = match.getMatchID();
-			let mplayers = match.getPlayers();
+			let thisMatch = m.active.get(ipp);
+			let matchid = thisMatch.getMatchID();
+			let mplayers = thisMatch.getPlayers();
+			// log(M + 'got match info');
 
 			// CHECK CLIENT FOR MATCH OVER MESSAGE SYNTAX!!
 			for (let i = 0; i < mplayers.length; i++) {
-				msg.reply(mplayers[i].channel, '[SOMETHING HERE]' + matchid);
+				msg.reply(mplayers[i].channel, 'mo~' + matchid);
 			}
+
+			// log(M + 'sent match over');
 
 			m.active.remove(ipp);
 			log(M + '[END] > ' + ipp, 'match');
@@ -84,9 +97,11 @@ function parseStatus(response) {
 // ipp: (string) ip:port combination
 // match: (match) match object
 m.add = function (ipp, match) {
+	log(M + 'Recieved match request: ' + match.getMatchID() + ', ' + match.getMatchIP() + ',  ' + match.getMatchPassword() + ', ' + match.getPlayers());
 	if (!m.active.has(ipp)) {
 		m.active.set(ipp, match);
 		log(M + '[START] > ' + ipp, 'match');
+
 		if (cfg.debug) {
 			log('matches.js -> m.add(' + ipp + ') -> true', 'debug');
 		}
@@ -116,6 +131,7 @@ m.checkActive = function (ipp) {
 		log('matches.js -> m.init()', 'debug');
 	}
 	m.jobCheckActive.start();
+	m.jobListActive.start();
 	log(M + 'STARTED JOB -> Check Active Matches');
 })();
 
